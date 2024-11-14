26c12e21fb84b39acae1db7120066a21f3cbaf73
azizarrahman <azizarrahman01032003@gmail.com>
2024-11-15 01:02:48 +0600

    Work eith button

--- src/Component/user/User.jsx
+++ src/Component/user/User.jsx
@@ -1,16 +1,23 @@
-import { Link } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 
 // eslint-disable-next-line react/prop-types
 const User = ({ singleuser }) => {
     // eslint-disable-next-line react/prop-types
     const { id, name, email, phone } = singleuser;
     console.log(singleuser);
+
+    const Navigate = useNavigate();
+
+    const handlshoedetails = () => {
+        console.log(id)
+        Navigate(`/user/${id}`)
+    }
     return (
         <div className="singleUser">
             <p2>{name}</p2>
             <p2>{email}</p2>
             <p2>{phone}</p2>
-            <Link to={`/user/${id}`}><button>Show details</button></Link>
+            <button onClick={handlshoedetails}>show details</button>
         </div>
     );
 };
